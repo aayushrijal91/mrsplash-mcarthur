5107c078d2b9e22bc8f375b53a528af4d9abe0d7
Aayush Rijal <arijal@aiims.com.au>
2022-10-25 23:14:48 +1100

    Homepage banner done

--- src/theme-files/parts/section-banner_home.php
+++ src/theme-files/parts/section-banner_home.php
@@ -6,6 +6,24 @@
             <div class="col-lg-6">
                 <div class="hero_heading fs-72 lh-1 fw-700"><?= $banner['heading'] ?></div>
                 <div class="sub_heading fs-30 lh-1"><?= $banner['sub_heading'] ?></div>
+                <?php if (have_rows('banner')) :
+                    while (have_rows('banner')) : the_row();
+                        if (have_rows('cta')) : ?>
+                            <div class="pt-5">
+                                <?php while (have_rows('cta')) : the_row();
+                                    $icon = get_sub_field('icon');
+                                    $title  = get_sub_field('title');
+                                ?>
+                                    <div class="row align-items-center pt-1 pb-2">
+                                        <div class="col-auto"><img src="<?= $icon['url'] ?>" alt="<?= $icon['alt'] ?>"></div>
+                                        <div class="col-auto fs-36 fw-700"><?= $title ?></div>
+                                    </div>
+                                <?php
+                                endwhile; ?>
+                            </div>
+                <?php endif;
+                    endwhile;
+                endif; ?>
             </div>
         </div>
     </div>
